05a55d2661ed32b04375d9cd4e97631f1b69d29e
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-09-27 15:26:14 +0700

    Update main.py

--- main.py
+++ main.py
@@ -33,9 +33,9 @@ async def kukiai(client: Client, message: Message):
   msg = message.text
   chat_id = message.chat.id
 
-  Kuki =   requests.get(f"https://kuki-api.tk/api/botname/owner/message={msg}").json()
+  Kuki =   requests.get(f"https://kuki-api.tk/api/testeraichatbot/IdzXartez/message={msg}").json()
 
-  Kuki1 =   requests.get(f"https://kuki-api.tk/api/message=hi").json()
+  Kuki =   requests.get(f"https://kuki-api.tk/api/message=hi").json()
 
   idz = f"{Kuki['reply']}"
       
