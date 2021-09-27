660ab029bc48f855175eb30cdf0cf489d522fe78
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-09-27 12:30:21 +0700

    Update main.py

--- main.py
+++ main.py
@@ -33,6 +33,8 @@ async def kukiai(client: Client, message: Message):
 
   Kuki =  requests.get(f"https://kuki-api.tk/api/message=hi").json()
 
+  Kuki =   requests.get(f"https://kuki-api.tk/api/testeraichatbot/IdzXartez/message={msg}").json()
+
   idz = f"{Kuki['reply']}"
       
   await client.send_chat_action(message.chat.id, "typing")
@@ -52,19 +54,13 @@ messagegroup = '''
 
 
 
-@kuki.on_message(filters.command("start"))
+@kuki.on_message(filters.command("alive"))
 async def start(_, message):
     self = await kuki.get_me()
     busername = self.username
     if message.chat.type != "public":
-        await message.reply_text(messagegroup)
-        return
-    else:
-        buttons = [[InlineKeyboardButton("Github", url="https://github.com/idzero23/ArtezidAI"),
-                    ]]
-        await message.reply_text(messagegroup, reply_markup=InlineKeyboardMarkup(buttons))
-
-
+      memek = await message.reply_text(messagegroup)
+      await memek.edit("Hello Im Online Now")
 
 
 kuki.run()
