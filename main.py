80258951d6c98c69e4366621e41225d31d3a3c4f
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-09-27 12:46:36 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from pyrogram.types import (
   Message, 
 )
 from pyrogram import filters, Client 
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 import requests
 import os
 import re
