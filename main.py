358a5ded411f324dadccfde95e09f0ca340328cc
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-09-27 13:14:16 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from pyrogram.types import (
   Message, 
 )
 from pyrogram import filters, Client 
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 import requests
 import os
 import re
-
 
 API_ID = os.environ.get("API_ID", None) 
 API_HASH = os.environ.get("API_HASH", None) 
@@ -33,8 +33,6 @@ async def kukiai(client: Client, message: Message):
 
   Kuki =  requests.get(f"https://kuki-api.tk/api/message=hi").json()
 
-  Kuki1 =   requests.get(f"https://kuki-api.tk/api/testeraichatbot/IdzXartez/message={msg}").json()
-
   idz = f"{Kuki['reply']}"
       
   await client.send_chat_action(message.chat.id, "typing")
@@ -42,6 +40,9 @@ async def kukiai(client: Client, message: Message):
 
 
 
+messageprivate = '''
+⚡️ 
+'''
 
 messagegroup = '''
 ⚡️ 
@@ -56,7 +57,13 @@ async def start(_, message):
     self = await kuki.get_me()
     busername = self.username
     if message.chat.type != "public":
-      await message.reply_text("messagegroup")
+        await message.reply_text(messagegroup)
+        return
+    else:
+        buttons = [[InlineKeyboardButton("Github", url="https://github.com/idzero23/ArtezidAI"),
+                    ]]
+        await message.reply_text(messagegroup, reply_markup=InlineKeyboardMarkup(buttons))
+
 
 
 
